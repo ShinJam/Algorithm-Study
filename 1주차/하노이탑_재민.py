d989a7ed3d2341ad0353e06000e5fa67ce94aa15
Shinjam <nevvjann@gmail.com>
2021-10-16 13:46:57 +0900

    Update 하노이탑_재민.py

--- 1주차/하노이탑_재민.py
+++ 1주차/하노이탑_재민.py
@@ -1,1 +1,14 @@
-print("sample code")
+ret = []
+def hanoi(n, frm, tmp, to):
+    global ret
+    if n == 0:
+        return
+    hanoi(n-1, frm, to, tmp)
+    ret.append((frm, to))
+    hanoi(n-1, tmp, frm, to)
+
+K = int(input())
+hanoi(K, 1, 2, 3)
+print(len(ret))
+for r in ret:
+    print(*r)
