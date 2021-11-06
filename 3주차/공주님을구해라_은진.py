4940945c1358b4ffd21533fd99b198be8d271cf7
aeroej <asd0755@naver.com>
2021-11-06 20:50:50 +0900

    Modify 공주님을구해라_은진.py

--- 3주차/공주님을구해라_은진.py
+++ 3주차/공주님을구해라_은진.py
@@ -19,7 +19,8 @@ def bfs(x, y):
     for i in range(4):
       nx = x + dx[i]
       ny = y + dy[i]
-      if (0 <= nx < n) and (0 <= ny < m) and visited[nx][ny] == 0:
+      
+      if (0 <= nx < n) and (0 <= ny < m) and not visited[nx][ny]:
         visited[nx][ny] = visited[x][y] + 1
 
         if graph[nx][ny] == 0:
