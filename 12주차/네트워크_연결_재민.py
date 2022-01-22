33c0f6424eb57344a8fedb58612350a372ac66a1
Shinjam <nevvjann@gmail.com>
2022-01-22 17:10:40 +0900

    Create 네트워크_연결_재민.py

---
+++ 12주차/네트워크_연결_재민.py
@@ -0,0 +1,33 @@
+import sys
+
+input = sys.stdin.readline
+
+def find(node):
+    if parent[node] != node:
+        parent[node] = find(parent[node])
+    return parent[node]
+
+
+def union(node_v, node_u):
+    v_root, u_root = find(node_v), find(node_u)
+    
+    if v_root != u_root:
+        parent[u_root] = v_root
+
+N = int(input())
+E = int(input())
+
+parent = {v:v for v in range(1, N+1)}
+edges = []
+for _ in range(E):
+    a, b, w = map(int, input().split())
+    edges.append((w, a, b))
+edges.sort()
+
+ans = 0
+for edge in edges:
+    w, v, u = edge
+    if find(v) != find(u):
+        union(v, u)
+        ans += w
+print(ans)
